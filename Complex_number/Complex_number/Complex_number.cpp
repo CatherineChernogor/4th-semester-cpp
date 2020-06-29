6a6ee21a64217a9090bf37d66bdc8572645f144b
Catherine <43898150+CatherineChernogor@users.noreply.github.com>
2020-06-29 19:14:49 +0800

    add complex

---
+++ Complex_number/Complex_number/Complex_number.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "complex.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Complex<double> n1(1, -2);
+    Complex<double> n2(4, 8);
+    cout << n1 << '\n' << n2 << '\n';
+
+    Complex<double> res = n1 / n2;
+    cout << res << '\n';
+
+    cout << n1.phase();
+}
---
+++ Complex_number/Complex_number/complex.h
@@ -0,0 +1,104 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#define PI 3.14159265
+using namespace std;
+
+template<class T> class Complex {
+
+public:
+
+	T re;
+	T im;
+
+	Complex();
+	Complex(T _re, T _im);
+	Complex(const Complex<T>& n);
+
+	//~Complex();
+
+	Complex<T>& operator =(const Complex<T>& n);
+	Complex<T>& operator +=(const Complex<T>& n);
+	Complex<T> operator +(const Complex<T>& n) const;
+	Complex<T>& operator -=(const Complex<T>& n);
+	Complex<T> operator -(const Complex<T>& n) const;
+	Complex<T> operator *(const Complex<T>& n) const;
+	Complex<T> operator /(const Complex<T>& n) const;
+
+	Complex<T> pow(int p);
+	double module();
+	double phase();
+
+	template <class> friend ostream& operator <<(ostream& out, Complex<T>& n);
+};
+
+template <class T> Complex<T>::Complex() {
+	this->re = 0;
+	this->im = 0;
+}
+template <class T> Complex<T>::Complex(T _re, T _im) {
+	this->re = _re;
+	this->im = _im;
+}
+template <class T> Complex<T>::Complex(const Complex<T>& n) {
+	this->re = n.re;
+	this->im = n.im;
+}
+
+template <class T> Complex<T>& Complex<T>::operator =(const Complex<T>& n) {
+	this->re = n.re;
+	this->im = n.im;
+	return *this;
+}
+template <class T> Complex<T>& Complex<T>::operator +=(const Complex<T>& n) {
+	this->re += n.re;
+	this->im += n.im;
+	return *this;
+}
+template <class T> Complex<T> Complex<T>::operator +(const Complex<T>& n) const {
+	Complex<T> res(*this);
+	return res += n;
+}
+template <class T> Complex<T>& Complex<T>::operator -=(const Complex<T>& n) {
+	this->re -= n.re;
+	this->im -= n.im;
+	return *this;
+}
+template <class T> Complex<T> Complex<T>::operator -(const Complex<T>& n) const {
+	Complex<T> res(*this);
+	return res -= n;
+}
+template <class T> Complex<T> Complex<T>::operator *(const Complex<T>& n) const {
+	Complex<T> res;
+	res.re = (this->re * n.re) - (this->im * n.im);
+	res.im = (this->re * n.im) + (this->im * n.re);
+	return res;
+}
+template <class T> Complex<T> Complex<T>::operator /(const Complex<T>& n) const {
+	Complex<T> res;
+	double conjugate = n.re * n.re + n.im * n.im;
+	res.re = (this->re * n.re + this->im * n.im) / conjugate;
+	res.im = (n.re * this->im - this->re * n.im) / conjugate;
+	return res;
+}
+template <class T> ostream& operator <<(ostream& out, Complex<T>& n) {
+	if (n.re) out << n.re;
+	else out << 0;
+
+	if (n.im >= 0) out << '+';
+
+	out << n.im<<'i';
+	
+	return out;
+}
+
+template <class T> Complex<T> Complex<T>::pow(int p) {
+
+}
+
+template <class T> double Complex<T>::module() {
+	return sqrt(this->re * this->re + this->im * this->im);
+}
+template <class T> double Complex<T>::phase() {
+	return 180 * atan(this->im / this->re) / PI;
+}
